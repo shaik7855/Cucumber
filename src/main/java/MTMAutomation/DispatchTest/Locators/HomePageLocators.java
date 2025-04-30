c268fa7f1ae871bd23efcbfba97d19edcac3631d
SaiPhanindra-Bhimanojjula <78841256+SaiPhanindra-Bhimanojjula@users.noreply.github.com>
2025-04-30 12:37:01 +0530

    Update HomePageLocators.java

--- src/main/java/MTMAutomation/DispatchTest/Locators/HomePageLocators.java
+++ src/main/java/MTMAutomation/DispatchTest/Locators/HomePageLocators.java
@@ -3,9 +3,9 @@ package MTMAutomation.DispatchTest.Locators;
 public class HomePageLocators 
 {
 	// --------------------------------Home Page Locators------------------------------------------- //
-	public static final String DISPATCH_TAB = "//a[@id='ember463']"; //xpath
-	public static final String lYFT_TAB = "//a[@id='ember496']"; //xpath
-	 public static final String Dispatch_Tab  = "//a[@id='ember463']"; // XPATH
+	public static final String DISPATCH_TAB = "//a[text()='Dispatch']"; //xpath
+	public static final String lYFT_TAB = "//a[text()='Lyft']"; //xpath
+	 public static final String Dispatch_Tab  = "//a[text()='Dispatch']"; // XPATH
 	public static final String LYFT_Tab  = "//a[text()='Lyft']"; // XPATH
-	public static final String OLOS_TAB = "//a[@id='ember521']"; //xpath
+	public static final String OLOS_TAB = "//a[text()='OLOS']"; //xpath
 }
